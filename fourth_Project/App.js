39164763518bd532badc93d6e289c2e391f6c208
RenatoLinard <renatolinardjr@gmail.com>
2023-07-12 10:27:16 -0400

    quarto projeto finalizado

--- fourth_Project/App.js
+++ fourth_Project/App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, StyleSheet, View, Image } from 'react-native';
+import congo from './assets/floresta-congo.png'
+import amazonica from './assets/floresta-amazonica.png'
+import taiga from './assets/floresta-taiga.png'
+
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={estilos.container}>
+      <Text style={estilos.titulo}>Florestas pelo mundo</Text>
+
+      <Image source={congo} style={estilos.img}/>
+      <Text style={estilos.legenda}>Floresta do congo (Africa)</Text>
+
+      <Image
+        source={taiga} style={estilos.img}/>
+      <Text style={estilos.legenda}>Floresta (Hemisfério norte)</Text>
+
+      <Image
+        source={amazonica} style={estilos.img}/>
+      <Text style={estilos.legenda}>Floresta amazonica (America do sul)</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+
+
+const estilos= StyleSheet.create({
+  titulo: {
+    fontSize: 40,
+    marginTop: 60,
+    marginBottom: 20,
   },
+
+  img: {
+    height: 180,
+    width: 360,
+  },
+
+  container: {
+    backgroundColor: 'lightgray',
+    flex:1,
+    alignItems: 'center',
+
+  },
+
+  legenda: {
+    marginBottom: 30,
+    fontSize: 20,
+  }
 });
