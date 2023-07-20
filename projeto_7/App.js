5a5a59d24189c8d88eaf5638be71efe863d6b3d0
RenatoLinard <renatolinardjr@gmail.com>
2023-07-20 18:45:54 -0400

    projeto_7 finalizado

--- projeto_7/App.js
+++ projeto_7/App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {View, Text, Image, StyleSheet} from 'react-native';
+import anthony from './assets/anthony.jpg'
+import sayo from './assets/sayo.jpg'
+import renato from './assets/renato.jpg'
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return(
+        <View style={estilos.container}>
+            <View style={estilos.card}>
+                <Text style={estilos.funcao}>Mobile Developer</Text>
+                <Text style={estilos.nome}>Renato Linard</Text>
+                <Image source= {renato} style={estilos.img}/>
+            </View>    
+            
+            <View style={estilos.card}>
+                <Text style={estilos.funcao}>FullStack Developer</Text>
+                <Text style={estilos.nome}>Sayonara Sousa</Text>
+                <Image source= {sayo} style={estilos.img}/>
+            </View>    
+            
+            <View style={estilos.card}>
+                <Text style={estilos.funcao}>Frontend Developer</Text>
+                <Text style={estilos.nome}>Anthony Linard</Text>
+                <Image source= {anthony} style={estilos.img}/>
+            </View>    
+
+        </View> 
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+
+const estilos = StyleSheet.create({
+    container: {
+        backgroundColor: 'lightblue',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+
+    img: {
+        width: 70,
+        height:70,
+        borderColor:'black',
+        borderWidth: 1,
+        borderRadius: 35,
+        marginTop:10,
+    },
+
+    card: {
+        backgroundColor: 'blue',
+        height: 170,
+        padding: 20,
+        width: 350,
+        borderRadius: 30,
+        borderColor: 'black',
+        borderWidth: 2,
+        marginBottom:20,
+    },
+
+    funcao: {
+        marginBottom:10,
+        fontSize: 20,
+        color: 'white'
+    },
+
+    nome: {
+        fontSize:13,
+        color: 'white',
+    },
 });
