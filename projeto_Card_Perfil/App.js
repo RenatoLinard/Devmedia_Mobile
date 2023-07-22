7e553d487f1758e39b8b1e4b51d0bca221913105
RenatoLinard <renatolinardjr@gmail.com>
2023-07-22 06:01:42 -0400

    projeto_Card_Perfil finalizado.

---
+++ projeto_Card_Perfil/App.js
@@ -0,0 +1,76 @@
+import React from "react";
+import {View, Text,Image, StyleSheet} from 'react-native';
+import familia from './assets/familia.jpg'
+import renato from './assets/renato.jpg'
+
+
+
+export default function App() {
+    return(
+        <View style={estilos.container}>
+            
+            <View style={estilos.boximg}>
+                <Image source={familia} style={estilos.img}/>
+            </View>
+
+            <View style={estilos.boxperfil}>
+                <Image source={renato} style={estilos.fotoperfil}/>
+
+                <Text style={estilos.titulo}> Renato Linard </Text>
+                
+                <Text style={estilos.descrição}> 
+                Formado em Quimica Industrial pela instituição CESFA,
+                atuo na area de caldeira há 13 anos. No momento estou
+                aprendendo a carreira mobile para um futuro proximo eu possa 
+                migrar para a area ou conciliar as duas profissões. </Text>    
+            </View> 
+        </View>
+    );
+}
+
+
+
+const estilos = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+
+    fotoperfil: {
+        width: 150,
+        height: 150,
+        borderColor: 'black',
+        borderWidth: 2,
+        borderRadius: 75,
+    },
+
+    img: {
+        width: 392,
+        height: 400,
+    },
+    
+    boximg: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    titulo: {
+        fontSize: 30,
+        color: 'black',
+        marginBottom: 15,
+        marginTop: 15,
+
+    },
+
+    descrição: {
+        fontSize: 15,
+        fontStyle: 'italic',
+        textAlign: 'center',
+    },
+
+    boxperfil: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
+});
+
