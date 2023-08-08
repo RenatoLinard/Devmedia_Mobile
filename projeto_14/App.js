cb1cd7f0842f135da0dec88765e48a8fbb33e93d
RenatoLinard <renatolinardjr@gmail.com>
2023-08-08 02:35:19 -0400

    projeto 14 (adicionado icons nas tabs)

--- projeto_14/App.js
+++ projeto_14/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Entypo } from '@expo/vector-icons'; 
 
 import Home from './componentes/Home';
 import Telas from './componentes/Telas';
 import Rotas from './componentes/Rotas';
 import Biblioteca from './componentes/Biblioteca';
+import { MaterialIcons } from '@expo/vector-icons'; 
+import { FontAwesome5 } from '@expo/vector-icons'; 
+import { Ionicons } from '@expo/vector-icons'; 
+
 
 const Tabs = createBottomTabNavigator()
 
@@ -13,10 +18,38 @@ export default function App() {
     return(
         <NavigationContainer>
             <Tabs.Navigator>
-                <Tabs.Screen name='Home' component={Home}/>
-                <Tabs.Screen name='Telas' component={Telas}/>
-                <Tabs.Screen name='Rotas' component={Rotas}/>
-                <Tabs.Screen name='Biblioteca' component={Biblioteca}/>
+                <Tabs.Screen 
+                    name='Home' 
+                    component={Home}
+                    options={{
+                        tabBarIcon: ({color}) => (
+                            <Entypo name="home" size={24} color={color} />
+                        ),
+                    }}/>
+                <Tabs.Screen 
+                    name='Telas'
+                    component={Telas}
+                    options={{
+                        tabBarIcon: ({color}) => (
+                            <MaterialIcons name="add-to-home-screen" size={25} color={color} />                            
+                        ),
+                    }}/>
+                <Tabs.Screen 
+                    name='Rotas' 
+                    component={Rotas}
+                    options={{
+                        tabBarIcon: ({color}) => (
+                            <FontAwesome5 name="route" size={24} color={color} />
+                        ),
+                    }}/>
+                <Tabs.Screen 
+                    name='Biblioteca' 
+                    component={Biblioteca}
+                    options={{
+                        tabBarIcon: ({color}) => (
+                            <Ionicons name="library" size={24} color={color} />
+                        ),
+                    }}/>
             </Tabs.Navigator>
         </NavigationContainer>
     )
