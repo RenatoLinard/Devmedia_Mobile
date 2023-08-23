b79bcc898c63fd78133fc2500a88dc5ae2b603ae
renatolinard <renatolinardjr@gmail.com>
2023-08-23 17:26:56 -0400

    projeto personalApp iniciado

---
+++ personalApp/components/rotaInicial/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+import logo from '../../assets/Personal.png';
+import styles from "./styles";
+
+
+export default function Inicial() {
+    return(
+        <View style={styles.container}>
+
+            <View style={styles.upper}>
+                 <Image
+                    source={logo}
+                    style={styles.logo}
+                
+                  />
+                <Text style={styles.titulo}> Bem Vindo!!! </Text>
+
+            </View>
+
+        </View>
+    )
+};
---
+++ personalApp/components/rotaInicial/styles.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#A6A6A6'
+    },
+
+    upper: {
+        alignItems: 'center',
+        backgroundColor: '#595959',
+        borderBottomLeftRadius: 40,
+        borderBottomRightRadius: 40,
+    },
+
+    logo: {
+        width: 330,
+        height: 300,
+    },
+
+    titulo: {
+        fontSize: 50,
+    }
+});
+
+
+export default styles;
